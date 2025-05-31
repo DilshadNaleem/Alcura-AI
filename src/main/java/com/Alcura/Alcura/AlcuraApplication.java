530208ff40f871a428299c61c9982ee7ad41a8cd
DilshadNaleem <dilshadnaleem13@gmail.com>
2025-06-01 05:13:11 +0530

    Initial Project setup

---
+++ src/main/java/com/Alcura/Alcura/AlcuraApplication.java
@@ -0,0 +1,13 @@
+package com.Alcura.Alcura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alcura/Alcura/AlcuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alcura.Alcura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
