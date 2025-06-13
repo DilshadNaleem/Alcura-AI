926abf12011921d166ed6b1bdb71775b46eea147
DilshadNaleem <dilshadnaleem13@gmail.com>
2025-06-13 09:55:01 +0530

    Admin Reset Password

--- src/main/java/com/Alcura/Admin/Controller/AdminHomeController.java
+++ src/main/java/com/Alcura/Admin/Controller/AdminHomeController.java
@@ -23,4 +23,16 @@ public class AdminHomeController {
     {
         return "/Admin/Admin_verification";
     }
+
+    @GetMapping("/Admin/RecoverPassword")
+    public String recoverPassword()
+    {
+        return "/Admin/RecoverPassword";
+    }
+
+    @GetMapping("/Admin/ResetPassword")
+    public String resetPassword()
+    {
+        return "/Admin/AdminResetPassword";
+    }
 }
